f4acc222ef66f0ff511a8310ab729ead0841e251
thirtyseven <thirtyseven@me.com>
2018-04-04 08:57:56 -0700

    Comments only

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
+  # Note that email must be case-insensitively unique.
+
   # Include default devise modules. Others available are:
   # :confirmable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable
-
-  # TODO(chandler37): do we look at uniqueness of email case-insensitively?
 end
